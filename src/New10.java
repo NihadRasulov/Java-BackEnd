556f4f1c7a56fba2c3cf681464507ec96db405ed
Nihat <nihad.rasulov2023@khazar.org>
2024-10-26 18:17:15 +0400

    New 10 commit new

--- src/New10.java
+++ src/New10.java
@@ -3,7 +3,7 @@ public class New10{
     public static void main(String[] args){
         Scanner number = new Scanner(System.in);
         int a = number.nextInt();
-        int b = number.nextInt();
+
 
     }
 }
