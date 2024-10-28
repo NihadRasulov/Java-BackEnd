3d093a6ea9669787824e46d6294ee9b628537f81
Nihat <nihad.rasulov2023@khazar.org>
2024-10-28 21:25:23 +0400

    Task3

---
+++ Turing Academy/src/Task3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        Scanner number = new Scanner(System.in);
+        System.out.println("Please enter the number of a: ");
+        int a = number.nextInt();
+        System.out.println("Please enter the number of b: ");
+        int b = number.nextInt();
+
+        a+=5;
+        b+=2;
+
+        System.out.println("The number of c: " + a);
+        System.out.println("The number of d: " + b);
+
+    }
+}
