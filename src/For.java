8cafaf72235d9fb179de2f7935a07646a8d72b3b
Nihat <nihad.rasulov2023@khazar.org>
2024-11-03 13:16:28 +0400

    ForLoop

--- src/For.java
+++ src/For.java
@@ -2,7 +2,6 @@ public class For {
 
     public static void main(String[] args) {
 
-            String name = "Nihad";
         for( int i=1;i<=3;i++){
             for(int j=1;j<=3;j++){
                 if(i==j){
